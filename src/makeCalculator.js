aae9f27ad0d75144b3af8c2f7a29a33ae59fea41
michelam-pseudo <michelammilian@outlook.com>
2025-06-24 14:59:00 -0300

    Concretização da sub-rotina makeCalculator requisitada na tarefa

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,34 @@
  */
 function makeCalculator() {
   // write code here
+
+  return {
+    result: 0,
+    add: function (number) {
+      this.result += number;
+
+      return this;
+    },
+    subtract: function (number) {
+      this.result = this.result - number;
+    },
+    multiply: function (number) {
+      this.result *= number;
+    },
+    divide: function (number) {
+      this.result = this.result / number;
+    },
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+    operate: function (callback, number) {
+      (() => callback.call(this, number))();
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
